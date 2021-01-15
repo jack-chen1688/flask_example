e44d2035b1f6d3f9ff25b8a50ce43c419c110d08
Xuehua Chen <xuehua@gmail.com>
2021-01-15 07:58:34 -0800

    rename main.py to app.py based on flask convention.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import socket
 
+container_hostname = socket.gethostname()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World'
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0')
